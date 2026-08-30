4659f4a1ae75656058e046e49fbfa6355ae9b76a
vanshkashya1984-source <vanshkashya1984@gmail.com>
2026-08-31 03:25:12 +0530

    Create build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,42 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
+}
+
+android {
+    namespace = "com.vanshkashya.liquidglass"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.vanshkashya.liquidglass"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildFeatures {
+        compose = true
+    }
+
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+    implementation(platform("androidx.compose:compose-bom:2025.08.00"))
+
+    implementation("androidx.activity:activity-compose:1.10.1")
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.foundation:foundation")
+    implementation("androidx.compose.material3:material3")
+
+    debugImplementation("androidx.compose.ui:ui-tooling")
+
+    implementation("com.kyant.backdrop:backdrop:1.3.0")
+}
